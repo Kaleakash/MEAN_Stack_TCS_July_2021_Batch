18c67fc6f2c6703b54e2123c06ab786718d2f228
akash <akash300383@gmail.com>
2021-08-17 19:35:22 +0530

    done

--- Angular Projects/angular-routing/src/app/app-routing.module.ts
+++ Angular Projects/angular-routing/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+// routing navigation rules 
+// http://localhost:4200
+
+const routes: Routes = [
+ {path:"aboutus",component:AboutUsComponent},
+ {path:"contactus",component:ContactUsComponent},
+ {path:"login",component:LoginComponent},
+ {path:"",redirectTo:"login",pathMatch:"full"},
+ {path:"**",component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular Projects/angular-routing/src/app/app.module.ts
+++ Angular Projects/angular-routing/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    LoginComponent,
+    DashboardComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
